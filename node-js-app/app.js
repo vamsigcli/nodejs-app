45fc5f725de8363921753c0bd659ff0b7b474edf
Vamsi Krishna <vamsichowdarykoppula123@gmail.com>
2026-04-10 18:20:11 +0530

    disabled tests to test rollback

--- node-js-app/app.js
+++ node-js-app/app.js
@@ -1,10 +1,10 @@
-throw new Error("Intentional failure for rollback test");
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from ECS Fargate v2');
+  // Intentionally crash the app for rollback test
+  throw new Error('Intentional failure for rollback test');
+  // res.send('Hello from ECS Fargate v2');
 });
 
 if (require.main === module) {
