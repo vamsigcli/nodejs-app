835a1df04061e2dff289e1a7df4d5cc0bf943885
Vamsi Krishna <vamsichowdarykoppula123@gmail.com>
2026-04-10 18:24:46 +0530

    enabled tests

--- node-js-app/app.js
+++ node-js-app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from ECS Fargate');
+  res.send('Hello from ECS Fargate v2');
 });
 
 if (require.main === module) {
